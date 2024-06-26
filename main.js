90cfdca2bf5ba4614b150027036dd11002b6d924
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-06-26 14:30:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// let son1 = +prompt ("son yoz")
+// let son2 = +prompt ("son yoz")
+
+// console.log (son1 * son2)
+
+// ism familiya yosh maktab
+
+// ism: Bekzod type: string
+
+let ism = prompt("ism yozing: ");
+let familya = prompt("familyangizni yozing: ");
+let yosh = prompt("yoshingizni yozing: ");
+let maktab = prompt("maktab yozing: ");
+
+
+console.log("ismi: " + ism + typeof ism )
+console.log("familyasi: " + familya )
+console.log("yoshi: " + yosh )
+console.log("maktab: " + maktab )
